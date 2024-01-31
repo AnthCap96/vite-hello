422a78886783960164fc3e749ccc9e4124d7d995
AnthCap96 <anthonycapoccia22@gmail.com>
2024-01-31 13:45:03 +0100

    create vite dcm end exercise
    create vite ended exercise

---
+++ vite-hello/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+  name: `App`,
+
+
+data() {
+  return {
+    message: "Anthony",
+    image: "",
+  }
+ },
+ methods: {
+
+ }
+}
+</script>
+
+<template>
+  <h1>Ciao {{ message }}</h1>
+  <img src="https://www.greenme.it/wp-content/uploads/2020/08/curioso-come-george.jpg">
+</template>
+
+<style>
+</style>
